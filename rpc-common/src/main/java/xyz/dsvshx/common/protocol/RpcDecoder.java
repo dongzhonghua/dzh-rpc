fda883ef747ef46ee400d99caa87fb422e534ef4
dongzhonghua <dongzhonghua03@kuaishou.com>
2021-03-03 18:15:40 +0800

    确定协议

---
+++ rpc-common/src/main/java/xyz/dsvshx/common/protocol/RpcDecoder.java
@@ -0,0 +1,38 @@
+package xyz.dsvshx.common.protocol;
+
+import java.util.List;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import xyz.dsvshx.common.protocol.serialize.Serializer;
+
+public class RpcDecoder extends ByteToMessageDecoder {
+    private Class<?> clazz;
+    private Serializer serializer;
+
+    public RpcDecoder(Class<?> clazz, Serializer serializer) {
+        this.clazz = clazz;
+        this.serializer = serializer;
+    }
+
+    @Override
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf in, List<Object> out)
+            throws Exception {
+        if (in.readableBytes() < 4) {
+            // 四个字节表示长度，如果收到的还不到四个字节的话直接返回
+            return;
+        }
+        in.markReaderIndex();
+        int dataLength = in.readInt();
+        if (in.readableBytes() < dataLength) {
+            in.resetReaderIndex();
+            return;
+        }
+        byte[] data = new byte[dataLength];
+        in.readBytes(data);
+
+        Object obj = serializer.deserialize(clazz, data);
+        out.add(obj);
+    }
+}
---
+++ rpc-common/src/main/java/xyz/dsvshx/common/protocol/RpcResponse.java
@@ -0,0 +1,19 @@
+package xyz.dsvshx.common.protocol;
+
+import lombok.Data;
+
+@Data
+public class RpcResponse {
+    /**
+     * 响应ID
+     */
+    private String requestId;
+    /**
+     * 错误信息
+     */
+    private String error;
+    /**
+     * 返回的结果
+     */
+    private Object result;
+}
