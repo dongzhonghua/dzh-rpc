1d917c0dae6022e1f24f3c675c02a5f686554cfb
dongzhonghua <dongzhonghua03@kuaishou.com>
2021-05-24 11:56:44 +0800

    fix

--- rpc-common/src/main/java/xyz/dsvshx/common/service/HelloService.java
+++ rpc-common/src/main/java/xyz/dsvshx/common/service/HelloService.java
@@ -6,7 +6,7 @@ import xyz.dsvshx.common.annotation.RpcInterface;
  * @author dongzhonghua
  * Created on 2021-03-03
  */
-@RpcInterface
+@RpcInterface // 用RpcService来代替
 public interface HelloService {
     String sayHello(String msg);
 }
